98701e056f527b218c7d99fdb780c9e1e4c96a22
verdantkyber <kingjalexa@gmail.com>
2018-10-08 23:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  object= delete object[key]
+  object= Object.assign({}, obj)
+  delete object[key]
   return object
 }
 
